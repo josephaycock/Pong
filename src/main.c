c8d6784d254a2f69a95bc8b8ee361fbd7d84856b
Joey Aycock <jaycock@Mac.attlocal.net>
2025-08-30 11:30:47 -0500

    Initial commit: Pong Setup

--- src/main.c
+++ src/main.c
@@ -12,14 +12,49 @@
 *  ./bin/build_osx
 */
 
+const int screenWidth = 1280;
+const int screenHeight = 720;
+const char* title = "Pong";
+
+typedef struct {
+    Vector2 position;
+    Vector2 size;
+    float speed;
+    Color color;
+} PaddleLeft;
+
+typedef struct {
+    Vector2 position;
+    Vector2 size;
+    float speed;
+    Color color;
+} PaddleRight;
+
+typedef struct {
+    Vector2 position;
+    float radius;
+    Vector2 velocity;
+    Color color;
+} Ball;
+
 int main(void) {
 
-    InitWindow(800, 600, "Template-5.5");
+    InitWindow(screenWidth, screenHeight, title);
+
+    PaddleLeft leftPaddle = { {0, (screenHeight / 2) - 76}, {48, 152}, 1.0, DARKGREEN};
+    PaddleRight rightPaddle = { {screenWidth - 48, (screenHeight / 2) - 76}, {48, 152}, 1.0, DARKGREEN};
+    Ball ball = { { (screenWidth / 2) - 9, (screenHeight / 2) - 9}, 18, {2, 2}, DARKGREEN};
+
+    SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
+        ClearBackground(LIME);
+
+        DrawRectangleV(leftPaddle.position, leftPaddle.size, leftPaddle.color);
+        DrawRectangleV(rightPaddle.position, rightPaddle.size, rightPaddle.color);
+        DrawCircleV(ball.position, ball.radius, ball.color);
 
         EndDrawing();
     }
